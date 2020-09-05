bdbd330ccb6e5681d7c04aa438650c6209894267
reinaldo luiz <reinaldolluiz@gmail.com>
2020-09-05 16:59:36 -0300

    Order priority tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Category.create(name: string)
+#Company.create(name: string, department: string, email:string)
+#Product.creat(name: string, price: double, description: string, photo: file, sellsman:references)
+#User.create(name:string, email:string, company: references, product: product)
+#Comment.create(title: string, comment: string, user: references)
+#WishList.create(wish: boolean, product: references)
+#Negociation.create(sellsman:references, status: integer, buyer:references, message: references )
+#Order.create(status: integer, negociation: references)
+
+
