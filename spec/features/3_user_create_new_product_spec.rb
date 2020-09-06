0198f2e1d9888a75e5ecee69b97cb7395cb2b99a
reinaldo luiz <reinaldolluiz@gmail.com>
2020-09-06 14:32:59 -0300

    Create Active Storage

--- spec/features/3_user_create_new_product_spec.rb
+++ spec/features/3_user_create_new_product_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 feature 'User create new product' do 
+  xscenario 'must be signed in' do 
+    #Arrange
+    #before_action :authenticate_user!, only: [:index]
+    #Act
+    visit root_path
+    click_on 'Categorias'
+    #Assert
+    expect(current_path).to eq new_user_session_path
+    expect(page).to have_content 'Para continuar, faça login ou registre-se'
+  end
+  
   xscenario 'sucessfully' do 
+    #Arrange
+    #Act
+    #Assert
   end
 end
