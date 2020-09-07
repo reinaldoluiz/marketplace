8da976ec0aa12f01a5237622029952a7af255334
reinaldo luiz <reinaldolluiz@gmail.com>
2020-09-07 00:30:44 -0300

    WIP Storage Imagens

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_06_173200) do
+ActiveRecord::Schema.define(version: 2020_09_06_222246) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 2020_09_06_173200) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.string "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -47,4 +57,5 @@ ActiveRecord::Schema.define(version: 2020_09_06_173200) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "products", "users"
 end
--- spec/features/3_user_create_new_product_spec.rb
+++ spec/features/3_user_create_new_product_spec.rb
@@ -1,20 +1,36 @@
 require 'rails_helper'
 
 feature 'User create new product' do 
-  xscenario 'must be signed in' do 
+  scenario 'must be signed in' do 
     #Arrange
-    #before_action :authenticate_user!, only: [:index]
     #Act
     visit root_path
-    click_on 'Categorias'
+    click_on 'Produtos'
     #Assert
     expect(current_path).to eq new_user_session_path
     expect(page).to have_content 'Para continuar, faça login ou registre-se'
   end
   
-  xscenario 'sucessfully' do 
+  scenario 'sucessfully' do 
     #Arrange
+    user = User.create!(name:'Silvio Santos', email:'silvio@sbt.com', password:'12345678')
+    product = Product.create!(name: 'Carro', price: 150.000, description: 'O carro da sua vida, não encontrará nada melhor!', user: user)
     #Act
+    login_as(user, scope: :user)
+    visit root_path
+    click_on 'Produtos'
+    click_on 'Criar novo produto'
+
+    fill_in 'Produto', with: 'Carro'
+    fill_in 'Preço', with: 'R$ 150.000,00'
+    fill_in 'Descrição', with: 'O carro da sua vida, não encontrará nada melhor!'
+
     #Assert
+    expect(current_path).to eq product_path(Product.last)
+    expect(page).to have_content('Carro')
+    expect(page).to have_content('R$ 150.000,00')
+    expect(page).to have_content('O carro da sua vida, não encontrará nada melhor!')
+    expect(page).to have_content('Silvio Santos')
+    expect(page).to have_link('Voltar')
   end
 end
